b7defed11f2891dd953b3b631b2db9f30467e69d
Vassiliy Pimkin <vassiliy.pimkin@gmail.com>
2017-03-01 11:47:46 +0300

    Hooks example clarified

--- source/spec/lib/examples/properties/wrapIt.coffee
+++ source/spec/lib/examples/properties/wrapIt.coffee
@@ -1,50 +1,54 @@
 'use strict'
 
 module.exports = (dd, that) ->
-  if typeof describe is 'function'
-    before ->
-      that.container = ''
-    after ->
+  if dd.helper.forMocha()
+    context 'With Mocha hooks', ->
+      before ->
+        that.container = ''
+      after ->
       delete that.container
-    context "with 'az'", ->
-      beforeEach ->
-        that.container += 'a'
-      afterEach ->
-        that.container += 'z'
-      context "with 'by'", ->
+      context "with 'az'", ->
         beforeEach ->
-          that.container += 'b'
+          that.container += 'a'
         afterEach ->
-          that.container += 'y'
-        context "with 'cx'", ->
+          that.container += 'z'
+        context "with 'by'", ->
           beforeEach ->
-            that.container += 'c'
+            that.container += 'b'
           afterEach ->
-            that.container += 'x'
-          dd.drive [
-            it: "wraps '1'"
-            i: [ 1 ]
-            e: 'abc|1|'
-          ,
-            it: "wraps '1' again"
-            i: [ 2 ]
-            e: 'abc|1|xyzabc|2|'
-          ,
-            it: "wraps '1' again and again"
-            i: [ 3 ]
-            e: 'abc|1|xyzabc|2|xyzabc|3|'
-          ]
-  else
-    that.container = ''
+            that.container += 'y'
+          context "with 'cx'", ->
+            beforeEach ->
+              that.container += 'c'
+            afterEach ->
+              that.container += 'x'
+            dd.drive [
+              it: "wraps '1'"
+              i: [ 1 ]
+              e: 'abc|1|'
+            ,
+              it: "wraps '1' again"
+              i: [ 2 ]
+              e: 'abc|1|xyzabc|2|'
+            ,
+              it: "wraps '1' again and again"
+              i: [ 3 ]
+              e: 'abc|1|xyzabc|2|xyzabc|3|'
+            ]
+  dd.context 'With ddry hooks', ->
     dd.drive [
       before: ->
-        that.container += 'abc'
-      after: ->
-        that.container += 'xyz'
-    ,
+        @.container = 'abc'
       it: "wraps '1'"
       i: [ 1 ]
       e: 'abc|1|'
+      after: ->
+        @.container += 'xyz'
+    ,
+      before: ->
+        @.container += 'abc'
+      after: ->
+        @.container += 'xyz'
     ,
       it: "wraps '1' again"
       i: [ 2 ]
@@ -54,5 +58,5 @@ module.exports = (dd, that) ->
       i: [ 3 ]
       e: 'abc|1|xyzabc|2|xyzabc|3|'
       after: ->
-        delete that.container
+        delete @.container
     ]
--- spec/lib/examples/properties/wrapIt.js
+++ spec/lib/examples/properties/wrapIt.js
@@ -2,66 +2,73 @@
 (function() {
   'use strict';
   module.exports = function(dd, that) {
-    if (typeof describe === 'function') {
-      before(function() {
-        return that.container = '';
-      });
-      after(function() {
-        return delete that.container;
-      });
-      return context("with 'az'", function() {
-        beforeEach(function() {
-          return that.container += 'a';
+    if (dd.helper.forMocha()) {
+      context('With Mocha hooks', function() {
+        before(function() {
+          return that.container = '';
         });
-        afterEach(function() {
-          return that.container += 'z';
-        });
-        return context("with 'by'", function() {
+        after(function() {});
+        delete that.container;
+        return context("with 'az'", function() {
           beforeEach(function() {
-            return that.container += 'b';
+            return that.container += 'a';
           });
           afterEach(function() {
-            return that.container += 'y';
+            return that.container += 'z';
           });
-          return context("with 'cx'", function() {
+          return context("with 'by'", function() {
             beforeEach(function() {
-              return that.container += 'c';
+              return that.container += 'b';
             });
             afterEach(function() {
-              return that.container += 'x';
+              return that.container += 'y';
             });
-            return dd.drive([
-              {
-                it: "wraps '1'",
-                i: [1],
-                e: 'abc|1|'
-              }, {
-                it: "wraps '1' again",
-                i: [2],
-                e: 'abc|1|xyzabc|2|'
-              }, {
-                it: "wraps '1' again and again",
-                i: [3],
-                e: 'abc|1|xyzabc|2|xyzabc|3|'
-              }
-            ]);
+            return context("with 'cx'", function() {
+              beforeEach(function() {
+                return that.container += 'c';
+              });
+              afterEach(function() {
+                return that.container += 'x';
+              });
+              return dd.drive([
+                {
+                  it: "wraps '1'",
+                  i: [1],
+                  e: 'abc|1|'
+                }, {
+                  it: "wraps '1' again",
+                  i: [2],
+                  e: 'abc|1|xyzabc|2|'
+                }, {
+                  it: "wraps '1' again and again",
+                  i: [3],
+                  e: 'abc|1|xyzabc|2|xyzabc|3|'
+                }
+              ]);
+            });
           });
         });
       });
-    } else {
-      that.container = '';
+    }
+    return dd.context('With ddry hooks', function() {
       return dd.drive([
         {
           before: function() {
-            return that.container += 'abc';
+            return this.container = 'abc';
           },
-          after: function() {
-            return that.container += 'xyz';
-          }
-        }, {
           it: "wraps '1'",
           i: [1],
-          e: 'abc|1|'
+          e: 'abc|1|',
+          after: function() {
+            return this.container += 'xyz';
+          }
+        }, {
+          before: function() {
+            return this.container += 'abc';
+          },
+          after: function() {
+            return this.container += 'xyz';
+          }
         }, {
           it: "wraps '1' again",
           i: [2],
@@ -71,11 +78,11 @@
           i: [3],
           e: 'abc|1|xyzabc|2|xyzabc|3|',
           after: function() {
-            return delete that.container;
+            return delete this.container;
           }
         }
       ]);
-    }
+    });
   };
 
 }).call(this);
